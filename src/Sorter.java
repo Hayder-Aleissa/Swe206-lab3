ab758da997ff43c51d252c3ad53786f07b2d29d5
Hayder <hmhaleissa@gmail.com>
2023-02-07 20:17:57 +0300

    this is your first time trying this

---
+++ src/Sorter.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Sorter{
+    public static void main(String[] args) {
+        System.out.println("Working Fine");
+
+        System.out.println("is it working?");
+    }
+
+}
